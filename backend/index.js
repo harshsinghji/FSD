2220f1a35dfd763c2c38ac3bee7c260fbebb8d8a
Harshdeep Singh <harshdeepsinghjnv@gmail.com>
2024-03-01 12:47:39 +0530

    01 march Updated

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,17 @@
 const express = require('express')
+const users = require('.src/users');
+
 require('dotenv').config()
 const app = express()
 
-console.log(process.env.port)
+//console.log(process.env.port)
 
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
+app.get('/api/users', users.getUsers ); //using to get all users
+
 app.listen(process.env.port, ()=>{
-    console.log("hello");
+    console.log("Listening.....",process.env.port);
 })
